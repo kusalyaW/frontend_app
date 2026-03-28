d7449ddd55a9dd457869b52a085bb2ee3d96cd69
kusalya <kusalyawidanagama@gmail.com>
2026-03-28 16:17:03 +0530

    Add frontend files

---
+++ gateway/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({ status: "ok" });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
